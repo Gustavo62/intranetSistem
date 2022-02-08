ca5149669a0d605827ec400a59936b7f68e27c8a
Gustavo62 <gust904@gmail.com>
2022-02-08 09:00:46 -0300

    corrigindo links css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Intra_sin_production"
 
   config.action_mailer.perform_caching = false
-
+  config.assets.prefix = "javascript/stylesheets"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
