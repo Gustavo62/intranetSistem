497465612d6ffee567ad8c9d89394903b0d743ba
Gustavo62 <gust904@gmail.com>
2022-02-08 15:21:54 -0300

    Trocando Layout dos emails de novo cad

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,8 @@ class UserMailer < ApplicationMailer
   def criado_com_sucesso_popup(user)
     @user       = user
     @associado  = Intranet::Associado.where(user_id: @user.id).take
-    @cartorio   = Intranet::Cartorio.find(@associado.intranet_cartorio_id)
-    @cidade     = Intranet::Cidade.find(@cartorio.intranet_cidade_id)
+    @cartorios  = Intranet::Cartorio.where(id: @associado.intranet_cartorio)
+    @cidades    = Intranet::Cidade.all 
     @nome_abrev = @user.nome.split(" ")
     @nome_abrev = @nome_abrev[0] + " " + @nome_abrev[1]
     mail to: "gust904@gmail.com", subject: "Novo Filiado Pendente de Liberação"
