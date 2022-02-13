b469f3fbddab7215a0dc92e329e70b4975b7268a
Gustavo62 <gust904@gmail.com>
2022-02-12 23:33:40 -0300

    last update for love of god

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,16 +63,7 @@ namespace :puma do
   after :start, :nginx_restart
 end
 namespace :deploy do 
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
+  desc "Make sure local git is in sync with remote." 
 
   desc 'Initial Deploy'
   task :initial do
@@ -88,8 +79,7 @@ namespace :deploy do
       invoke 'puma:restart'
     end
   end
-
-  before :starting,     :check_revision
+ 
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
