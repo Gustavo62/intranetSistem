2c32b78050bc0cbdf8274ba22d146d96dde23a5c
Gustavo62 <gust904@gmail.com>
2022-03-18 10:03:04 -0300

    fix all errors heroku finaly

--- db/migrate/20220318114737_add_content_type_to_admins.rb
+++ db/migrate/20220318114737_add_content_type_to_admins.rb
@@ -2,5 +2,7 @@ class AddContentTypeToAdmins < ActiveRecord::Migration[6.1]
   def change
     add_column :admins, :avatar_content_type, :string
     add_column :admins, :avatar_file_name, :string 
+    add_column :admins, :avatar_file_size, :string 
+    add_column :admins, :avatar_updated_at, :string  
   end
 end
