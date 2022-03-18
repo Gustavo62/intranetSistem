3b2193bf22de7d632f3e6bb79596221c06ce8f8f
Gustavo62 <gust904@gmail.com>
2022-03-17 22:19:13 -0300

    fix all errors heroku finaly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.assets.precompile += %w(creative/manifest.js creative/manifest.css images/* bootstrap/* fonts/* magnific-popup/* morphext/* owlcarousel2/* rs-plugin-5/* stylesheets/* javascripts/* config/*)
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
-  config.assets.digest = true
+  config.assets.digest = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
