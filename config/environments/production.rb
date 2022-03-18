0042059a91782611c0f9892b10069da7c7d60184
Gustavo62 <gust904@gmail.com>
2022-03-18 00:26:06 -0300

    fix all errors heroku finaly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,11 +114,12 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.paperclip_defaults = {
     storage: :s3,
+    s3_protocol: :https,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
-  }
+  } 
 end
