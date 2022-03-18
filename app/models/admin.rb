f71a27497417544b88c99583b6aeb35e22d21fcd
Gustavo62 <gust904@gmail.com>
2022-03-18 02:55:14 -0300

    fix all errors heroku finaly

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,8 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :validatable, :authentication_keys => [:cpf]
   validates :nome,:cpf, :email,:password_confirmation,:privilegio_id, presence: true 
   
-  has_one_attached :avatar
+  has_one_attached :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :privilegios, :class_name => 'Intranet::Privilegio'
   has_many :departamentos, :class_name => 'Intranet::Departamento'
 	def avatar_thumbnail
